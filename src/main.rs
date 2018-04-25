5c9206e404f078c27d62807a171c4109a05f421a
Jon Craton <jncraton@gmail.com>
2018-04-25 01:14:55 -0400

    Removed rustc-serialize and docopt dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 #![cfg(not(test))]
 
-extern crate docopt;
 extern crate iota;
 extern crate libc;
 extern crate rustbox;
-extern crate rustc_serialize;
 
-use docopt::Docopt;
 use iota::{Editor, Input};
 use rustbox::{InitOptions, InputMode, OutputMode, RustBox};
 use std::io::stdin;
-static USAGE: &'static str = "
-Usage: iota [<filename>] [options]
-       iota --help
-
-Options:
-    -h, --help                     Show this message.
-";
-
-#[derive(RustcDecodable, Debug)]
-struct Args {
-    arg_filename: Option<String>,
-    flag_help: bool,
-}
+use std::env;
 
 fn is_atty(fileno: libc::c_int) -> bool {
     // FIXME: find a way to do this without unsafe
@@ -31,16 +16,12 @@ fn is_atty(fileno: libc::c_int) -> bool {
 }
 
 fn main() {
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
-
     let stdin_is_atty = is_atty(libc::STDIN_FILENO);
     let stderr_is_atty = is_atty(libc::STDERR_FILENO);
 
     // editor source - either a filename or stdin
     let source = if stdin_is_atty {
-        Input::Filename(args.arg_filename)
+        Input::Filename(env::args().nth(1))
     } else {
         Input::Stdin(stdin())
     };
